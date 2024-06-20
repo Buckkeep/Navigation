18e142922b71514299e2445bb517a25fdbefa409
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-06-20 14:09:13 +0100

    Navigating to different data types using NavigationPath

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -8,15 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var path = NavigationPath()
+    
     var body: some View {
-        NavigationStack {
-            List(0..<100) { i in
-                NavigationLink("Select \(i)", value: i)
+        NavigationStack(path: $path) {
+            List {
+                ForEach(0..<5) { i in
+                    NavigationLink("Select Number: \(i)", value: i)
+                }
+                ForEach(0..<5) { i in
+                    NavigationLink("Select String: \(i)", value: String(i))
+                }
             }
+            .toolbar {
+                Button("Push 245") {
+                    path.append(245)
+                }
+                
+                Button("Push hello") {
+                    path.append("hello")
+                }
+             }
             .navigationDestination(for: Int.self) {selection in
-                Text("You selected \(selection)")
+                Text("You selected number \(selection)")
+            }
+            .navigationDestination(for: String.self) {selection in
+                Text("You selected string \(selection)")
             }
         }
+
     }
 }
 
