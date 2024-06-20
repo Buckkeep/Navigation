40a88ee3e25e15957e3ac5e7715c058f3a055e1c
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-06-20 10:51:04 +0100

    Initial Commit

---
+++ Navigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Navigation
+//
+//  Created by Buhecha, Neeta (Trainee Engineer) on 20/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Buhecha, Neeta (Trainee Engineer) on 20/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
